903aa3be9e41a45f166921a88c0da7f14e0438fe
David Till <126269055+dtill8945@users.noreply.github.com>
2023-04-25 13:19:21 -0500

    Update index.js
    Update to file, added in pseudocode which better explains my functions and their purposes.

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const sarah = {
     refills: 1,
     subscription: true,
     coupon: false,
-}
+};
 
 const rocky = {
     prescription: "phenylephrine",
@@ -20,18 +20,36 @@ const rocky = {
     refills: 5,
     subscription: true,
     coupon: true,
-}
+};
+// Create function which will calculate total with or without a subscription
+function subscriptionFunction(pricePerRefill, refills, subscription) {
+    let firstTotal = pricePerRefill * refills;
+    let subscriptionTotal = firstTotal;
+    if (subscription === true) { // If sub is true we will apply a 25% discount
+        subscriptionTotal = firstTotal - (firstTotal * 0.25);
+    }
+    return subscriptionTotal;
+};
+// Create function which will calculate our total with or without a coupon
+function couponFunction(subscriptionTotal, coupon) {
+    let finalTotal = subscriptionTotal;
+    if (coupon === true) { // If coupon is true we will remove $10 from our SubscriptionTotal
+        finalTotal = subscriptionTotal - 10;
+    }
+    return finalTotal;
+};
+// Our main function will use our two sub functions to calculate and print the final total in a string
+function mainFunction(person) {
+    let subscriptionTotal = subscriptionFunction( // Calculates subscription total
+        person.pricePerRefill,
+        person.refills,
+        person.subscription
+    );
 
-function subscriptionStatus()
+    let finalTotal = couponFunction(subscriptionTotal, person.coupon); // Calculates our final total
+    console.log(`Your grand total is $${finalTotal}`);
+};
 
-function couponStatus()
-
-function mainFunction(name){
-    let subscriptionFunction = subscriptionStatus(name.pricePerRefill, name.refills, name.subscription)
-    let couponFunction = couponStatus(name.pricePerRefill, name.refills, name.coupon, name.subscriptionTotal)
-    console.log(subTotal)
-}
-
-mainFunction(timmy)
-mainFunction(sarah)
-mainFunction(rocky)
+mainFunction(timmy);
+mainFunction(sarah);
+mainFunction(rocky);
